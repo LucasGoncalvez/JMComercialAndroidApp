82e9ded2ff8d1d37f4f4f3254b477f301b249c69
LucasGoncalvez <goncalvezlucas281@gmail.com>
2023-07-09 23:59:29 -0400

    Inicio de la mejora de la pantalla de Abm Cliente: Se agregó la imagen cargar/error

--- app/src/main/java/com/example/jmcomercialapp/a_ui/modulos/clientes/abm/ABMCliente.kt
+++ app/src/main/java/com/example/jmcomercialapp/a_ui/modulos/clientes/abm/ABMCliente.kt
@@ -1,16 +1,10 @@
 package com.example.jmcomercialapp.a_ui.modulos.clientes.abm
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
-import android.widget.Button
-import android.widget.PopupWindow
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.jmcomercialapp.R
@@ -23,7 +17,7 @@ class ABMCliente : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.lista_clientes_fragment)
         binding = FragmentAbmClienteBinding.inflate(inflater, container, false)
         return binding.root
@@ -35,26 +29,26 @@ class ABMCliente : Fragment() {
     }
 
     fun btnCiudadAction(){
-        val inflater = LayoutInflater.from(requireContext())
-        val popupView = inflater.inflate(R.layout.popup_cities, null)
-
-        val popupWindow = PopupWindow(
-            popupView,
-            WindowManager.LayoutParams.WRAP_CONTENT,
-            WindowManager.LayoutParams.WRAP_CONTENT
-        )
-
-        popupWindow.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        popupWindow.showAtLocation(binding.btnSelectCity, Gravity.CENTER, 0, 0)
-
-        popupWindow.setOnDismissListener {
-            //TODO: Perform any actions upon dismissal of the popup window
-        }
-
-        val closeButton = popupView.findViewById<Button>(R.id.cancelSelectCityButton)
-        closeButton.setOnClickListener{
-            popupWindow.dismiss()
-        }
+//        val inflater = LayoutInflater.from(requireContext())
+//        val popupView = inflater.inflate(R.layout.popup_cities, null)
+//
+//        val popupWindow = PopupWindow(
+//            popupView,
+//            WindowManager.LayoutParams.WRAP_CONTENT,
+//            WindowManager.LayoutParams.WRAP_CONTENT
+//        )
+//
+//        popupWindow.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//        popupWindow.showAtLocation(binding.btnSelectCity, Gravity.CENTER, 0, 0)
+//
+//        popupWindow.setOnDismissListener {
+//            //TODO: Perform any actions upon dismissal of the popup window
+//        }
+//
+//        val closeButton = popupView.findViewById<Button>(R.id.cancelSelectCityButton)
+//        closeButton.setOnClickListener{
+//            popupWindow.dismiss()
+//        }
     }
 
     fun btnAddGeolocationAction(){
